a2f88b2d5eb356c4fa13af4b0cdc2a0fc8b342a7
Kirill remote <kirillmorgunov@mail.ru>
2021-04-19 15:22:59 +0300

    делает intro слайдером

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,9 @@
 import {contentSlider} from './modules/slider';
+import {hotelSlider} from './modules/hotel-slider';
 import {bookmarks} from './modules/bookmarks';
 import {initHeader} from './modules/header';
 
 contentSlider();
+hotelSlider();
 bookmarks();
 initHeader();
---
+++ source/js/modules/hotel-slider.js
@@ -0,0 +1,57 @@
+const hotelSlider = () => {
+    const breakpointSm = window.matchMedia('(max-width: 767px)');
+    const hotelSliderElem = document.querySelector('.hotel-slider__list');
+  
+    if (!hotelSliderElem) {
+      return;
+    }
+   
+    const hotelSliderSettings = {
+      slidesPerView: 1,   
+  
+      pagination: {
+        el: '.slider-pagination',
+        type: 'bullets',
+        clickable: true,
+      },
+  
+      navigation: {
+        nextEl: '.slider-controls-btn--next',
+        prevEl: '.slider-controls-btn--prev',
+        disabledClass: 'slider-controls-btn--disabled',
+      },
+  
+    //   breakpoints: {      
+    //     768: {
+    //       slidesPerView: 2,
+    //       spaceBetween: 33,
+    //     },
+    //     320: {
+    //       slidesPerView: 'auto',
+    //       pagination: {
+    //         el: '.slider-pagination',
+    //         type: 'progressbar',
+    //       },
+    //     },
+    //   },
+  
+      slideClass: 'hotel-slider__item',
+      slideActiveClass: 'hotel-slider__item--active',
+      slideNextClass: 'hotel-slider__item--next',
+      slidePrevClass: 'hotel-slider__item--prev',
+      wrapperClass: 'hotel-slider__wrap',
+    };
+  
+    window.hotelSlider = new Swiper(hotelSliderElem, hotelSliderSettings);
+  
+    const breakpointChecker = () => {
+      window.hotelSlider.destroy();
+      window.hotelSlider = new Swiper(hotelSliderElem, hotelSliderSettings);
+    };
+    breakpointSm.addListener(breakpointChecker);
+    breakpointChecker();
+  };
+  
+  
+  export {hotelSlider};
+  
